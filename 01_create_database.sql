8d3c75d452e582c145f5bb06b401fb1e63fdfe04
Carol Iantas <iantascarol@outlook.com>
2025-10-03 22:28:21 -0600

    Update 01_create_database.sql

--- 01_create_database.sql
+++ 01_create_database.sql
@@ -2,11 +2,6 @@
    File: prepared_queries.sql
    Project: SKS National Bank (Final Project) =D
    Group Members: <Pedro Molina, Carol Iantas and Vivekbhai Pateli>
-   
-   Notes:
-     - This file assumes the BankDB schema from create_database.sql exists.
-     - Each query is a stored procedure or function with a short description
-       and an example test call at the end of its block.
 */
 
 /* 
